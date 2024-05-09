1a4cc21a7732257eab6d12d364cf5a8e1359d0e1
duy8go <truonganhduy2k2@gmail.com>
2024-05-09 22:59:45 +0700

    abc

--- study_in_pink2.h
+++ study_in_pink2.h
@@ -45,53 +45,6 @@ enum ItemType { MAGIC_BOOK, ENERGY_DRINK, FIRST_AID, EXCEMPTION_CARD, PASSING_CA
 enum ElementType { PATH, WALL, FAKE_WALL };
 enum RobotType { C=0, S, W, SW };
 
-class MapElement {
-protected:
-    ElementType type;
-    int r;
-    int c;
-public:
-    MapElement(ElementType in_type){
-        type = in_type;
-    };
-    virtual ~MapElement(){};
-    virtual ElementType getType() const{
-        return type;
-    };
-};
-
-class Path : public MapElement {
-    public:
-    Path(): MapElement(PATH){}
-};
-
-class Wall : public MapElement {
-    public:
-    Wall():MapElement(WALL){}
-};
-
-class FakeWall : public MapElement {
-    private:
-        int req_exp;
-    public:
-        FakeWall(int in_req_exp): MapElement(FAKE_WALL), req_exp(in_req_exp){
-            in_req_exp = ( r *257 + c*139+89)%900 + 1;
-        }
-        int getRepExp() const{
-            return req_exp;
-        }
-};
-
-class Map {
-private:
-    int num_rows, num_cols;
-    MapElement*** map;
-public:
-    Map(int num_rows, int num_cols, int num_walls, Position * array_walls, int num_fake_walls, Position * array_fake_walls) : num_rows(num_rows), num_cols(num_cols){};
-    ~Map(){};
-    bool isValid ( const Position & pos , MovingObject * mv_obj ) const{} ;
-};
-
 class Position {
 private:
     int r, c;
@@ -126,6 +79,40 @@ public:
     };
 };
 
+class MapElement {
+protected:
+    ElementType type;
+public:
+    MapElement(ElementType in_type){
+        type = in_type;
+    };
+    virtual ~MapElement(){};
+    virtual ElementType getType() const{
+        return type;
+    };
+};
+
+class Path : public MapElement {
+    public:
+    Path(): MapElement(PATH){}
+};
+
+class Wall : public MapElement {
+    public:
+    Wall():MapElement(WALL){}
+};
+
+class FakeWall : public MapElement {
+    private:
+        int req_exp;
+    public:
+        FakeWall(int in_req_exp): MapElement(FAKE_WALL), req_exp(in_req_exp){}
+        
+        int getRepExp() const{
+            return req_exp;
+        }
+};
+class Map;
 class MovingObject {
 protected:
     int index;
@@ -147,224 +134,262 @@ public:
     virtual void move() = 0;
     virtual string str() const = 0;
 };
-
-class Sherlock: public MovingObject /* TODO */ {
+class MovingObject;
+class Map {
 private:
-    // TODO
-    int hp;
-    int exp;
-    string moving_rule;
+    int num_rows, num_cols;
+    MapElement **map;
 public:
-    Sherlock(int index, const string & moving_rule, const Position & init_pos, Map * map, int init_hp, int init_exp)
-    :MovingObject(index, init_pos,map,"Sherlock"), hp(init_hp),exp(init_exp),moving_rule(moving_rule){
-        if ( hp > 500) hp = 500;
-        if ( hp <= 0) hp = 0;
-        if ( exp > 900) exp = 900;
-        if ( exp <= 0) exp = 0;
-    };
-    Position getNextPosition(){
-        static int c_index = 0;
-        char a = moving_rule[c_index++];
-        if (c_index == moving_rule.length()){
-            c_index = 0;
-        }
-        Position next_pos = getCurrentPosition();
-        if ( a == 'L'){
-            next_pos.setCol(next_pos.getCol()-1);
-        }
-        if ( a == 'R'){
-            next_pos.setCol(next_pos.getCol() +1);
-        }
-        if (a == 'U'){
-            next_pos.setRow(next_pos.getRow()+1);
-        }
-        if ( a == 'D'){
-            next_pos.setCol(next_pos.getRow()-1);
-        }
-        if ( map->isValid(next_pos, this)){
-            return next_pos;
-        }else return Position::npos;
-        
-    }
-    void move(){
-        Position next_pos = getNextPosition();
-        if ( next_pos != Position::npos ){
-            pos =  next_pos;
-        }
-    }
-    string str(){
-        return "Sherlock[index=" + to_string(index) + ";" + "pos=" + to_string(pos) + ";moving_rule=" + moving_rule +"]";
-    }
-    // ...
-};
-
-class Watson: public MovingObject /* TODO */ {
-private:
-    // TODO
-    int hp;
-    int exp;
-    string moving_rule;
-public:
-    Watson(int index, const string & moving_rule, const Position & init_pos, Map * map, int init_hp, int init_exp)
-    :MovingObject(index, init_pos,map,"Watson"), hp(init_hp),exp(init_exp),moving_rule(moving_rule){
-        if ( hp > 500) hp = 500;
-        if ( hp <= 0) hp = 0;
-        if ( exp > 900) exp = 900;
-        if ( exp <= 0) exp = 0;
-    };
-    Position getNextPosition(){
-        static int c_index = 0;
-        char a = moving_rule[c_index++];
-        if (c_index == moving_rule.length()){
-            c_index = 0;
-        }
-        Position next_pos = getCurrentPosition();
-        if ( a == 'L'){
-            next_pos.setCol(next_pos.getCol()-1);
-        }
-        if ( a == 'R'){
-            next_pos.setCol(next_pos.getCol() +1);
-        }
-        if (a == 'U'){
-            next_pos.setRow(next_pos.getRow()+1);
-        }
-        if ( a == 'D'){
-            next_pos.setCol(next_pos.getRow()-1);
-        }
-        if ( map->isValid(next_pos, this)){
-            return next_pos;
-        }else return Position::npos;
-        
-    }
-    void move(){
-        Position next_pos = getNextPosition();
-        if ( next_pos != Position::npos ){
-            pos =  next_pos;
-        }
-    }
-    string str(){
-        return "Watson[index=" + to_string(index) + ";" + "pos=" + to_string(pos) + ";moving_rule=" + moving_rule +"]";
-    }
-    // getNextPosition
-    // move
-    // str
-    // ...
-};
-int Manhattan(Position p1, Position p2){
-    return abs(p1.getRow()-p2.getRow()) + abs(p1.getCol()-p2.getCol());
-}
-class Criminal:public MovingObject /* TODO */ {
-private:
-    // TODO
-    
-public:
-    Criminal(int index, const Position & init_pos, Map * map, Sherlock * sherlock, Watson * watson)
-    :MovingObject(index, pos, map,"Criminal"){};
-    Position getNextPosition(){
-        // move U
-        Position next_pos_U;
-        next_pos_U.setRow(init_pos.getRow()+1);
-        next_pos_U.setCol(inti_pos.getCol())
-        int d_U = Manhattan(next_pos_U, sherlock.getNextPosition())
-    }
-    void move(){
-        Position next_pos = getNextPosition();
-        if ( next_pos != Position::npos ){
-            pos =  next_pos;
-        }
-    }
-    string str(){
-        return "Criminal[index=" + to_string(index) + ";" + "pos=" + to_string(pos)+"]";
-    }
-    
-    // ...
-};
-
-class ArrayMovingObject {
-private:
-    // TODO
-
-public:
-    ArrayMovingObject(int capacity);
-
-    ~ArrayMovingObject() ;
-    bool isFull() const;
-    bool add(MovingObject * mv_obj);
-    MovingObject * get(int index) const;
-    int size() const; // return current number of elements in the array
-    string str() const;
-};
-
-class Configuration {
-    friend class StudyPinkProgram;
-
-private:
-    // TODO
-
-public:
-    Configuration(const string & filepath);
-    ~Configuration();
-    string str() const;
-};
-
-// Robot, BaseItem, BaseBag,...
-
-class StudyPinkProgram {
-private:
-    // Sample attributes
-    Configuration * config;
-    
-    Sherlock * sherlock;
-    Watson * watson;
-    Criminal * criminal;
-    
-    Map * map;
-    ArrayMovingObject * arr_mv_objs;
-
-
-public:
-    StudyPinkProgram(const string & config_file_path);
-
-    bool isStop() const;
-
-    void printResult() const {
-        if (sherlock->getCurrentPosition().isEqual(criminal->getCurrentPosition())) {
-            cout << "Sherlock caught the criminal" << endl;
-        }
-        else if (watson->getCurrentPosition().isEqual(criminal->getCurrentPosition())) {
-            cout << "Watson caught the criminal" << endl;
-        }
-        else {
-            cout << "The criminal escaped" << endl;
-        }
-    }
-
-    void printStep(int si) const {
-        cout << "Step: " << setw(4) << setfill('0') << si
-            << "--"
-            << sherlock->str() << "--|--" << watson->str() << "--|--" << criminal->str() << endl;
-    }
-
-    void run(bool verbose) {
-        // Note: This is a sample code. You can change the implementation as you like.
-        // TODO
-        for (int istep = 0; istep < config->num_steps; ++istep) {
-            for (int i = 0; i < arr_mv_objs->size(); ++i) {
-                arr_mv_objs->get(i)->move();
-                if (isStop()) {
-                    printStep(istep);
-                    break;
-                }
-                if (verbose) {
-                    printStep(istep);
-                }
+    Map(int num_rows, int num_cols, int num_walls, Position * array_walls, int num_fake_walls, Position * array_fake_walls) : num_rows(num_rows), num_cols(num_cols){
+        map = new MapElement*[num_rows];
+        for ( int i = 0; i < num_rows; i++){
+            map[i]= new MapElement*[num_cols];
+            for ( int j = 0;j < num_cols; j++){
+                map[i][j] =  nullptr;
             }
         }
-        printResult();
-    }
+        for ( int i = 0; i < num_walls; i++){
+            int r = array_walls[i].getRow();
+            int c = array_walls[i].getCol();
+            map[r][c] = MapElement(WALL);
+        }
+        for ( int i =0; i < num_fake_walls; i++){
+            int r = array_fake_walls[i].getRow();
+            int c = array_fake_walls[i].getCol();
+            map[r][c] = MapElement(FAKE_WALL);
+            FakeWall((r*257+c*139+89)%900 + 1);
+        }
 
-    ~StudyPinkProgram();
+    };
+    ~Map(){
+        for (int i = 0; i < num_rows; i++){
+            for (int j = 0; j < num_cols; j++){
+                delete map[i][j];
+            }
+            delete[] map[i];
+        }
+        delete[] map;
+    }
+    bool isValid ( const Position & pos , MovingObject * mv_obj ) const{} ;
 };
+
+// class Sherlock: public MovingObject /* TODO */ {
+// private:
+//     // TODO
+//     int hp;
+//     int exp;
+//     string moving_rule;
+// public:
+//     Sherlock(int index, const string & moving_rule, const Position & init_pos, Map * map, int init_hp, int init_exp)
+//     :MovingObject(index, init_pos,map,"Sherlock"), hp(init_hp),exp(init_exp),moving_rule(moving_rule){
+//         if ( hp > 500) hp = 500;
+//         if ( hp <= 0) hp = 0;
+//         if ( exp > 900) exp = 900;
+//         if ( exp <= 0) exp = 0;
+//     };
+//     Position getNextPosition(){
+//         static int c_index = 0;
+//         char a = moving_rule[c_index++];
+//         if (c_index == moving_rule.length()){
+//             c_index = 0;
+//         }
+//         Position next_pos = getCurrentPosition();
+//         if ( a == 'L'){
+//             next_pos.setCol(next_pos.getCol()-1);
+//         }
+//         if ( a == 'R'){
+//             next_pos.setCol(next_pos.getCol() +1);
+//         }
+//         if (a == 'U'){
+//             next_pos.setRow(next_pos.getRow()+1);
+//         }
+//         if ( a == 'D'){
+//             next_pos.setCol(next_pos.getRow()-1);
+//         }
+//         if ( map->isValid(next_pos, this)){
+//             return next_pos;
+//         }else return Position::npos;
+        
+//     }
+//     void move(){
+//         Position next_pos = getNextPosition();
+//         if ( next_pos != Position::npos ){
+//             pos =  next_pos;
+//         }
+//     }
+//     string str(){
+//         return "Sherlock[index=" + to_string(index) + ";" + "pos=" + to_string(pos) + ";moving_rule=" + moving_rule +"]";
+//     }
+//     // ...
+// };
+
+// class Watson: public MovingObject /* TODO */ {
+// private:
+//     // TODO
+//     int hp;
+//     int exp;
+//     string moving_rule;
+// public:
+//     Watson(int index, const string & moving_rule, const Position & init_pos, Map * map, int init_hp, int init_exp)
+//     :MovingObject(index, init_pos,map,"Watson"), hp(init_hp),exp(init_exp),moving_rule(moving_rule){
+//         if ( hp > 500) hp = 500;
+//         if ( hp <= 0) hp = 0;
+//         if ( exp > 900) exp = 900;
+//         if ( exp <= 0) exp = 0;
+//     };
+//     Position getNextPosition(){
+//         static int c_index = 0;
+//         char a = moving_rule[c_index++];
+//         if (c_index == moving_rule.length()){
+//             c_index = 0;
+//         }
+//         Position next_pos = getCurrentPosition();
+//         if ( a == 'L'){
+//             next_pos.setCol(next_pos.getCol()-1);
+//         }
+//         if ( a == 'R'){
+//             next_pos.setCol(next_pos.getCol() +1);
+//         }
+//         if (a == 'U'){
+//             next_pos.setRow(next_pos.getRow()+1);
+//         }
+//         if ( a == 'D'){
+//             next_pos.setCol(next_pos.getRow()-1);
+//         }
+//         if ( map->isValid(next_pos, this)){
+//             return next_pos;
+//         }else return Position::npos;
+        
+//     }
+//     void move(){
+//         Position next_pos = getNextPosition();
+//         if ( next_pos != Position::npos ){
+//             pos =  next_pos;
+//         }
+//     }
+//     string str(){
+//         return "Watson[index=" + to_string(index) + ";" + "pos=" + to_string(pos) + ";moving_rule=" + moving_rule +"]";
+//     }
+//     // getNextPosition
+//     // move
+//     // str
+//     // ...
+// };
+// int Manhattan(Position p1, Position p2){
+//     return abs(p1.getRow()-p2.getRow()) + abs(p1.getCol()-p2.getCol());
+// }
+// class Criminal:public MovingObject /* TODO */ {
+// private:
+//     // TODO
+    
+// public:
+//     Criminal(int index, const Position & init_pos, Map * map, Sherlock * sherlock, Watson * watson)
+//     :MovingObject(index, pos, map,"Criminal"){};
+//     Position getNextPosition(){
+//         // move U
+//         Position next_pos_U;
+//         next_pos_U.setRow(init_pos.getRow()+1);
+//         next_pos_U.setCol(inti_pos.getCol())
+//         int d_U = Manhattan(next_pos_U, sherlock.getNextPosition())
+//     }
+//     void move(){
+//         Position next_pos = getNextPosition();
+//         if ( next_pos != Position::npos ){
+//             pos =  next_pos;
+//         }
+//     }
+//     string str(){
+//         return "Criminal[index=" + to_string(index) + ";" + "pos=" + to_string(pos)+"]";
+//     }
+    
+//     // ...
+// };
+
+// class ArrayMovingObject {
+// private:
+//     // TODO
+
+// public:
+//     ArrayMovingObject(int capacity);
+
+//     ~ArrayMovingObject() ;
+//     bool isFull() const;
+//     bool add(MovingObject * mv_obj);
+//     MovingObject * get(int index) const;
+//     int size() const; // return current number of elements in the array
+//     string str() const;
+// };
+
+// class Configuration {
+//     friend class StudyPinkProgram;
+
+// private:
+//     // TODO
+
+// public:
+//     Configuration(const string & filepath);
+//     ~Configuration();
+//     string str() const;
+// };
+
+// // Robot, BaseItem, BaseBag,...
+
+// class StudyPinkProgram {
+// private:
+//     // Sample attributes
+//     Configuration * config;
+    
+//     Sherlock * sherlock;
+//     Watson * watson;
+//     Criminal * criminal;
+    
+//     Map * map;
+//     ArrayMovingObject * arr_mv_objs;
+
+
+// public:
+//     StudyPinkProgram(const string & config_file_path);
+
+//     bool isStop() const;
+
+//     void printResult() const {
+//         if (sherlock->getCurrentPosition().isEqual(criminal->getCurrentPosition())) {
+//             cout << "Sherlock caught the criminal" << endl;
+//         }
+//         else if (watson->getCurrentPosition().isEqual(criminal->getCurrentPosition())) {
+//             cout << "Watson caught the criminal" << endl;
+//         }
+//         else {
+//             cout << "The criminal escaped" << endl;
+//         }
+//     }
+
+//     void printStep(int si) const {
+//         cout << "Step: " << setw(4) << setfill('0') << si
+//             << "--"
+//             << sherlock->str() << "--|--" << watson->str() << "--|--" << criminal->str() << endl;
+//     }
+
+//     void run(bool verbose) {
+//         // Note: This is a sample code. You can change the implementation as you like.
+//         // TODO
+//         for (int istep = 0; istep < config->num_steps; ++istep) {
+//             for (int i = 0; i < arr_mv_objs->size(); ++i) {
+//                 arr_mv_objs->get(i)->move();
+//                 if (isStop()) {
+//                     printStep(istep);
+//                     break;
+//                 }
+//                 if (verbose) {
+//                     printStep(istep);
+//                 }
+//             }
+//         }
+//         printResult();
+//     }
+
+//     ~StudyPinkProgram();
+// };
 
 ////////////////////////////////////////////////
 /// END OF STUDENT'S ANSWER
